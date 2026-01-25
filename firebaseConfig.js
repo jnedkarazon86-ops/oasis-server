d826d8eade9db1006682940a2de930fb3d4df904
jnedkarazon86-ops <jnedkarazon86@gmail.com>
2026-01-25 12:30:37 +0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,21 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth"; // أضفنا هذا السطر
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSy...", // مفتاحك هنا
-  authDomain: "oasis-app.firebaseapp.com",
-  projectId: "oasis-app",
-  storageBucket: "oasis-app.appspot.com",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyBKPGFbYjqEacHoU61DiUYq4QJzHENVUE0", // مفتاحك الفعلي الذي أرسلته
+  authDomain: "oasis-app-afa66.firebaseapp.com",
+  projectId: "oasis-app-afa66",
+  storageBucket: "oasis-app-afa66.firebasestorage.app",
+  messagingSenderId: "382600269975",
+  appId: "1:382600269975:android:d4fbc3e410dc0652cfd9d2"
 };
 
-// تشغيل Firebase
 const app = initializeApp(firebaseConfig);
-
-// تصدير قاعدة البيانات
 export const db = getFirestore(app);
-
-// تصدير نظام المصادقة (مهم جداً للتحقق بالإيميل)
-export const auth = getAuth(app); 
+export const auth = getAuth(app);
