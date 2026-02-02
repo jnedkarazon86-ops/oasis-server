f466715d11b4e28b6c161426b3837bd81abe9c7a
jnedkarazon86-ops <jnedkarazon86@gmail.com>
2026-02-02 11:51:18 +0300

    Update Constants.js

--- mobile/Constants.js
+++ mobile/Constants.js
@@ -1,5 +1,6 @@
+// mobile/Constants.js
+
 export const OASIS_KEYS = {
-  // Firebase Configuration
   FIREBASE: {
     apiKey: "AIzaSyBKPGFbYjqEacHoU61DiUYq4QJzHENVUE0",
     authDomain: "oasis-app-afa66.firebaseapp.com",
@@ -8,17 +9,16 @@ export const OASIS_KEYS = {
     messagingSenderId: "382600269975",
     appId: "1:382600269975:android:d4fbc3e410dc0652cfd9d2"
   },
-  
-  // ZegoCloud Configuration
   ZEGO: {
     appID: 1773421291,
     appSign: "48f1a163421aeb2dfdf57ac214f51362d8733ee19be92d3745a160a2521de2d7"
   },
-
-  // Profit Settings (الإعلانات)
-  AD_REFRESH_RATE: 60000, // كل دقيقة
+  AD_REFRESH_RATE: 60000, 
   PROFIT_LINKS: [
-    "https://www.highrevenuegate.com/example1", // ضع روابطك الحقيقية هنا
-    "https://www.highrevenuegate.com/example2"
+    "https://www.effectivegatecpm.com/pv5wwvpt?key=d089e046a8ec90d9b2b95e7b32944807", 
+    "https://otieu.com/4/10520849",                                                
+    "https://www.effectivegatecpm.com/qrjky2k9d7?key=0eeb59c5339d8e2b8a7f28e55e6d16a2", 
+    "https://www.effectivegatecpm.com/g5j4wjcf?key=0c62848e4ddf4458b8d378fe3132bbaf", 
+    "https://www.effectivegatecpm.com/denseskhi?key=8e442518041da6a96a35ad2f7275ed15"  
   ]
 };
