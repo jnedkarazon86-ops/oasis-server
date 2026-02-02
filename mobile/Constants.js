8ff0d8c4c0d0d419ecea6e19a30062f78b90af37
jnedkarazon86-ops <jnedkarazon86@gmail.com>
2026-02-02 11:34:13 +0300

    Create Constants.js

---
+++ mobile/Constants.js
@@ -0,0 +1,24 @@
+export const OASIS_KEYS = {
+  // Firebase Configuration
+  FIREBASE: {
+    apiKey: "AIzaSyBKPGFbYjqEacHoU61DiUYq4QJzHENVUE0",
+    authDomain: "oasis-app-afa66.firebaseapp.com",
+    projectId: "oasis-app-afa66",
+    storageBucket: "oasis-app-afa66.firebasestorage.app",
+    messagingSenderId: "382600269975",
+    appId: "1:382600269975:android:d4fbc3e410dc0652cfd9d2"
+  },
+  
+  // ZegoCloud Configuration
+  ZEGO: {
+    appID: 1773421291,
+    appSign: "48f1a163421aeb2dfdf57ac214f51362d8733ee19be92d3745a160a2521de2d7"
+  },
+
+  // Profit Settings (الإعلانات)
+  AD_REFRESH_RATE: 60000, // كل دقيقة
+  PROFIT_LINKS: [
+    "https://www.highrevenuegate.com/example1", // ضع روابطك الحقيقية هنا
+    "https://www.highrevenuegate.com/example2"
+  ]
+};
