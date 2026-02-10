8fc62b79a58ee4859be156830152930bd0501906
jnedkarazon86-ops <jnedkarazon86@gmail.com>
2026-02-10 16:18:12 +0300

    Update firebaseConfig.js

--- mobile/firebaseConfig.js
+++ mobile/firebaseConfig.js
@@ -1,16 +1,21 @@
+// firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBKPGFbYjqEacHoU61DiUYq4QJzHENVUE0", // مفتاحك الفعلي الذي أرسلته
-  authDomain: "oasis-app-afa66.firebaseapp.com",
-  projectId: "oasis-app-afa66",
-  storageBucket: "oasis-app-afa66.firebasestorage.app",
-  messagingSenderId: "382600269975",
-  appId: "1:382600269975:android:d4fbc3e410dc0652cfd9d2"
-};
+// استيراد الإعدادات المركزية من ملف Constants لضمان التنسيق ومنع التكرار
+import { OASIS_KEYS } from './Constants'; 
 
+/**
+ * تهيئة Firebase باستخدام المفاتيح الموحدة.
+ * ملاحظة: تم سحب الإعدادات من OASIS_KEYS.FIREBASE لتجنب 
+ * وجود قيم مكتوبة يدوياً (Hardcoded) كما هو مطلوب في تحليل المشروع.
+ */
+const firebaseConfig = OASIS_KEYS.FIREBASE;
+
+// بدء تشغيل تطبيق Firebase
 const app = initializeApp(firebaseConfig);
+
+// تصدير قواعد البيانات وخدمة التوثيق لاستخدامها في بقية التطبيق
 export const db = getFirestore(app);
 export const auth = getAuth(app);
